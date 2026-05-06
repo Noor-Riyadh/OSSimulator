b1f8918b6d08be65af8b642cd823bdbbeb6704f8
Manar <manarazzam959@outlook.com>
2026-05-07 02:28:05 +0300

    Implemented LRU algorithms

--- src/memory/LRU.java
+++ src/memory/LRU.java
@@ -7,14 +7,52 @@ import java.util.*;
 
 public class LRU {
     public static PageReplacementResult simulate(int[] referenceString, int numFrames) {
-        // Steps:
-        // 1. Use a Map<Integer, Integer> to track page → last used time (index)
-        // 2. For each page:
-        //    - If page in map → hit, update its last used time
-        //    - If not in map → fault
-        //      * If free frame → load directly
-        //      * If full → evict the page with the MINIMUM last used time
-        // 3. Record each step using result.addStep(new Step(...))
-        return null; // replace this
+        
+         PageReplacementResult result = new PageReplacementResult("LRU");
+        int[] frames = new int[numFrames];
+        Arrays.fill(frames, -1);
+  
+        Map<Integer, Integer> lastUsed = new LinkedHashMap<>();
+        for (int i = 0; i < referenceString.length; i++) {
+ 
+            int page = referenceString[i]; 
+            boolean isPageFault;
+            int replacedPage = -1;
+            if (lastUsed.containsKey(page)) {
+                isPageFault = false;
+                lastUsed.put(page, i);
+            } else {
+                isPageFault = true;
+                if (lastUsed.size() < numFrames) {
+                    for (int f = 0; f < numFrames; f++) {
+                        if (frames[f] == -1) {
+                            frames[f] = page;
+                            break;
+                        }
+                    }
+                } else {
+                    int lruPage = Collections.min(
+                        lastUsed.entrySet(),
+                        Map.Entry.comparingByValue()
+                    ).getKey();
+ 
+                    replacedPage = lruPage;
+                    lastUsed.remove(lruPage);
+                    for (int f = 0; f < numFrames; f++) {
+                        if (frames[f] == lruPage) {
+                            frames[f] = page;
+                            break;
+                        }
+                    }
+                }
+  
+                lastUsed.put(page, i);
+            }
+  
+            result.addStep(new Step(i + 1, page, Arrays.copyOf(frames, frames.length), isPageFault, replacedPage));
+        }
+ 
+    
+        return result  ; 
     }
 }
